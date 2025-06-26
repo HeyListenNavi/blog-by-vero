dfec434bc312e7ca777548a241c2dfeab2b32cf5
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2025-06-26 04:13:43 -0700

    Add nullables to User table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            
+
             $table->string('name');
-            $table->text('description');
-            $table->string('avatar_color');
-            $table->string('favorite_color');
-            $table->string('favorite_fruit');
+            $table->text('description')->nullable();
+            $table->string('avatar_color')->nullable();
+            $table->string('favorite_color')->nullable();
+            $table->string('favorite_fruit')->nullable();
 
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
