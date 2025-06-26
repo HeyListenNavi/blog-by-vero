e7d71dd08a7bf845778c0a80e2d22f0543caa495
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2025-06-26 03:55:53 -0700

    Update User migration and model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'description',
+        'avatar_color',
+        'favorite_color',
+        'favorite_fruit',
         'password',
         'role',
     ];
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            
             $table->string('name');
+            $table->text('description');
+            $table->string('avatar_color');
+            $table->string('favorite_color');
+            $table->string('favorite_fruit');
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
             $table->enum('role', ['Admin', 'User']);
         });
 
