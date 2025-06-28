ab1334a3dbff47d6e02524ada531c7a7dddc5c9e
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2025-06-28 04:16:55 -0700

    Fix window titlebar styling issue

--- resources/views/components/window.blade.php
+++ resources/views/components/window.blade.php
@@ -12,7 +12,7 @@ $buttonMap = [
 @endphp
 
 <div {{ $attributes->merge(["class" => "flex flex-col bg-background-primary shadow-window-outline p-2 min-w-42"]) }}>
-    <div id="titlebar" class="flex justify-between items-center p-2 bg-background-secondary select-none">
+    <div id="titlebar" class="flex justify-between gap-1 items-center p-2 bg-background-secondary select-none">
         <div>
             {{ $title }}
         </div>
