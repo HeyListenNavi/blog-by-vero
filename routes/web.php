2033cd526d0ed5392e0d7496a5f35fe7c5561462
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2025-07-23 01:53:23 -0700

    Fix Logout route

--- routes/web.php
+++ routes/web.php
@@ -59,5 +59,5 @@ Route::middleware('auth')->group(function () {
         ->name('comment.store');
 });
 
-Route::post('/logout', [AuthController::class, 'logout'])
+Route::post('/logout', [UserController::class, 'logout'])
     ->name('logout');
