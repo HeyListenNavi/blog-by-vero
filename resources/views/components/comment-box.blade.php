7ca2880a790bb125d145d40df27897223d425f18
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2025-07-23 02:05:53 -0700

    Add submission on enter in comment box

--- resources/views/components/comment-box.blade.php
+++ resources/views/components/comment-box.blade.php
@@ -2,7 +2,7 @@
     'action' => ''
 ])
 
-<form action="{{ $action }}" method="POST" class="flex flex-col gap-2">
+<form action="{{ $action }}" method="POST" class="flex flex-col gap-2" x-data>
     @csrf
     @if($errors->any())
         @foreach ($errors->all() as $error)
@@ -12,8 +12,14 @@
 
     <fieldset>
         <label for="comment">Comment</label>
-        <textarea class="w-full border-[3px] p-2 border-foreground resize-none" placeholder="Enter your comment here" type="text" name="comment"></textarea>
+        <textarea
+            class="w-full border-[3px] p-2 border-foreground resize-none"
+            placeholder="Enter your comment here"
+            type="text"
+            name="comment"
+            x-on:keydown.enter.prevent="$el.form.requestSubmit($refs.submitButton)"
+        ></textarea>
     </fieldset>
 
-    <x-button class="self-end" type="submit">Comment</x-button>
+    <x-button class="self-end" type="submit" x-ref="submitButton">Comment</x-button>
 </form>
