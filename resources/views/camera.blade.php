7da221ffe11f9562d346099fd7ef998c3fc8ffe3
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2025-08-21 22:39:58 -0700

    Make Camera Roll section responsive

--- resources/views/camera.blade.php
+++ resources/views/camera.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('body')
-<main class="p-2 h-screen grid grid-rows-[auto_1fr_auto] items-center gap-4">
-    <header class="px-4 flex flex-col gap-1">
+<main class="min-h-screen w-screen p-2 grid grid-rows-[auto_1fr_auto] items-center gap-4 bg-background-primary">
+    <header class="p-4 flex flex-col gap-1">
         <h1 class="text-body-large">Vero's camera roll</h1>
         <h2>veronica has a bunch of cameras, here, all of the photographies she has ever taken have been listed into different camera rolls</h2>
     </header>
 
-    <section class="mx-auto max-w-md">
+    <section class="px-2 mx-auto max-w-md">
         @forelse ($photographyPosts as $post)
         <article class="flex flex-col gap-8 items-center justify-center">
             <div class="flex flex-col gap-4 items-center">
@@ -22,7 +22,7 @@
         @endforelse
     </section>
 
-    <section>
+    <section class="p-4">
         {{ $photographyPosts->links('components.pagination', ['direction' => 'vertical']) }}
     </section>
 </main>
