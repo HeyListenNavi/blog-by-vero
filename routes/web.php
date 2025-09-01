7fd874051b3e2835e5cb695752dd8ca96385abac
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2025-08-31 23:17:24 -0700

    Clean routes

--- routes/web.php
+++ routes/web.php
@@ -11,45 +11,31 @@ Route::get('/', [PagesController::class, 'home'])
     ->name('home');
 
 Route::get('/about-me', [PagesController::class, 'aboutMe'])
-    ->name('about.me');
-
-Route::get('/camera', [PhotographyPostController::class, 'index'])
-    ->name('camera');
-
-Route::get('/journal', [PostController::class, 'index'])
-    ->name('journal');
+    ->name('aboutme');
 
 Route::get('/terminal', [PagesController::class, 'terminal'])
     ->name('terminal');
 
-Route::get('/community', [UserController::class, 'index'])
-    ->name('community');
 
-Route::get('/camera-roll/{photographyPost}', [PhotographyPostController::class, 'show'])
-    ->name('camera.roll');
-
-Route::get('/journal/{post}', [PostController::class, 'show'])
-    ->name('journal.post');
-
-Route::get('/comments', [CommentController::class, 'index'])
-    ->name('comments');
-
-Route::get('{user}/profile', [UserController::class, 'show'])
-    ->name('profile');
 
 Route::middleware('guest')->group(function () {
+    Route::get('/auth', [PagesController::class, 'auth'])
+        ->name('auth');
+    
     Route::post('/register', [UserController::class, 'register'])
         ->name('register.submit');
 
     Route::post('/login', [UserController::class, 'login'])
         ->name('login.submit');
-
-    Route::get('/auth', [PagesController::class, 'auth'])
-        ->name('auth');
 });
 
+Route::post('/logout', [UserController::class, 'logout'])
+    ->name('logout');
+
+
+
 Route::middleware('auth')->group(function () {
-    Route::post('/comments/{model}/{id}', [CommentController::class, 'store'])
+    Route::post('/comments/{model}/comments', [CommentController::class, 'store'])
         ->name('comment.store');
 
     Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])
@@ -62,5 +48,25 @@ Route::middleware('auth')->group(function () {
         ->name('profile.edit');
 });
 
-Route::post('/logout', [UserController::class, 'logout'])
-    ->name('logout');
+
+
+Route::get('/posts', [PostController::class, 'index'])
+    ->name('journal');
+
+Route::get('/posts/{post}', [PostController::class, 'show'])
+    ->name('journal.post');
+
+Route::get('/photography-posts', [PhotographyPostController::class, 'index'])
+        ->name('camera');
+
+Route::get('/photography-posts/{photographyPost}', [PhotographyPostController::class, 'show'])
+    ->name('camera.roll');
+
+Route::get('/users', [UserController::class, 'index'])
+    ->name('community');
+
+Route::get('/users/{user}', [UserController::class, 'show'])
+    ->name('profile');
+
+Route::get('/comments', [CommentController::class, 'index'])
+    ->name('comments');
