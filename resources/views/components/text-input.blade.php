f0e629ebb271d67dee93c2318ee881bcb167c783
Veronica Guzman Ochoa <heylistennavi@proton.me>
2025-09-24 17:27:34 -0700

    Add show password button

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -11,23 +11,30 @@
 
 <fieldset
     {{ $attributes->merge(['class' => 'flex flex-col gap-1']) }}
+    x-data="{ showPassword: false }"
 >
     <label for="user">{{ $label }}</label>
-    <input
-        class="w-full border-[3px] border-foreground focus:border-highlight p-2 outline-none"
-        @isset($placeholder)
-        placeholder="{{ $placeholder }}"
-        @endisset
-        type="{{ $type }}"
-        name="{{ $name }}"
-        @if ($required)
-        required
+    <div class="flex items-stretch">
+        <input
+            class="w-full border-[3px] border-foreground focus:border-highlight p-2 outline-none"
+            @isset($placeholder)
+            placeholder="{{ $placeholder }}"
+            @endisset
+            type="{{ $type }}"
+            x-bind:type="showPassword ? 'text' : 'password'"
+            name="{{ $name }}"
+            @if ($required)
+            required
+            @endif
+            @if ($autofocus)
+            autofocus
+            @endif
+            @isset($value)
+            value="{{ $value }}"
+            @endisset
+        >
+        @if($type == "password")
+        <x-button type="button" class="font-emoji text-3xl" x-on:click="showPassword = !showPassword" x-text="showPassword ? '3' : '2'"></x-button>
         @endif
-        @if ($autofocus)
-        autofocus
-        @endif
-        @isset($value)
-        value="{{ $value }}"
-        @endisset
-    >
+    </div>
 </fieldset>
