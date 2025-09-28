43966b9deaca923960992cc104d52dede3f32a26
Verónica <heylistennavi@proton.me>
2025-09-28 12:38:15 -0700

    Update camera-roll.blade.php

--- resources/views/layouts/camera-roll.blade.php
+++ resources/views/layouts/camera-roll.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('body')
-<main class="min-w-lg w-full bg-background-primary min-h-svh p-8 flex flex-col gap-4">
+<main class="min-w-0 min-w-lg w-full bg-background-primary min-h-svh p-4 flex flex-col gap-4">
     <a class="self-start underline cursor-pointer hover:text-highlight-secondary transition-colors" onclick="history.back()">← go back to the camera</a>
 
     <section class="max-w-4xl self-center flex flex-col gap-2">
-        <h1 class="text-display-medium text-4xl">{{ $photographyPost->title }}</h1>
+        <h1 class="text-display-medium text-lg lg:text-4xl">{{ $photographyPost->title }}</h1>
         <p>{{ $photographyPost->description }}</p>
     </section>
 
