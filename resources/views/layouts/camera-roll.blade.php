c67ee1e2578780456771f1c0894ce15aa0c5914a
Veronica Guzman Ochoa <heylistennavi@proton.me>
2025-09-28 03:31:26 -0700

    Fix photography posts photographies not showing bug

--- resources/views/layouts/camera-roll.blade.php
+++ resources/views/layouts/camera-roll.blade.php
@@ -3,7 +3,7 @@
 @section('body')
 <main class="min-w-lg w-full bg-background-primary min-h-svh p-8 flex flex-col gap-4">
     <a class="self-start underline cursor-pointer hover:text-highlight-secondary transition-colors" onclick="history.back()">← go back to the camera</a>
-    
+
     <section class="max-w-4xl self-center flex flex-col gap-2">
         <h1 class="text-display-medium text-4xl">{{ $photographyPost->title }}</h1>
         <p>{{ $photographyPost->description }}</p>
@@ -12,7 +12,7 @@
     <section class="max-w-4xl w-full self-center grid grid-cols-2 gap-4">
         @foreach ($photographyPost->photographies as $photography)
             <figure class="flex flex-col gap-2 items-center">
-                <img src="{{ $photography->path }}">
+                <img src="{{ asset('/storage' . $photography->path) }}">
                 <figcaption>{{ $photography->title }}</figcaption>
             </figure>
         @endforeach
