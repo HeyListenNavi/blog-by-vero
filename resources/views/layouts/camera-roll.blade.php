4f7374e665bcc26539151d569bba86113acc4cd6
Veronica Guzman Ochoa <heylistennavi@proton.me>
2025-09-28 03:32:59 -0700

    Fix storage asset typo in photography posts

--- resources/views/layouts/camera-roll.blade.php
+++ resources/views/layouts/camera-roll.blade.php
@@ -12,7 +12,7 @@
     <section class="max-w-4xl w-full self-center grid grid-cols-2 gap-4">
         @foreach ($photographyPost->photographies as $photography)
             <figure class="flex flex-col gap-2 items-center">
-                <img src="{{ asset('/storage' . $photography->path) }}">
+                <img src="{{ asset('storage/' . $photography->path) }}">
                 <figcaption>{{ $photography->title }}</figcaption>
             </figure>
         @endforeach
