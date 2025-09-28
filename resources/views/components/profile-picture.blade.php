ef29e1c65733669b5102da2d57a0b672f0bd968e
Veronica Guzman Ochoa <heylistennavi@proton.me>
2025-09-28 03:08:11 -0700

    Fix user profile picture misalignment problem

--- resources/views/components/profile-picture.blade.php
+++ resources/views/components/profile-picture.blade.php
@@ -2,6 +2,8 @@
     'letter'
 ])
 
-<div class="md:size-32 size-20 shrink-0 flex items-center justify-center bg-highlight-secondary/80 md:text-7xl text-5xl font-emoji select-none">
-    {{ $letter }}
+<div class="relative md:size-32 size-20 shrink-0 flex items-center justify-center bg-highlight-secondary/80 md:text-7xl text-5xl font-emoji select-none">
+    <span class="text-center absolute left-1/2 top-1/2 -translate-x-[40%] -translate-y-1/2">
+        {{ $letter }}
+    </span>
 </div>
