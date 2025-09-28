e2cca43e038d74b159f4487f1662a933f9ab3bdb
Veronica Guzman Ochoa <heylistennavi@proton.me>
2025-09-28 09:12:52 -0700

    Fix photography post migration

--- database/migrations/2025_04_17_090908_create_photography_posts_table.php
+++ database/migrations/2025_04_17_090908_create_photography_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('photography_posts', function (Blueprint $table) {
             $table->id();
             $table->text('title');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->foreignId('icon_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
