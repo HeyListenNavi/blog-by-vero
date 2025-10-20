5802460d712fa5ca2c2ee97f3bad3be184427fa6
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2025-10-20 02:27:49 -0700

    Update camera rolls layout

--- resources/views/camera.blade.php
+++ resources/views/camera.blade.php
@@ -1,29 +1,28 @@
 @extends('layouts.app')
 
 @section('body')
-<main class="min-h-screen w-screen p-2 grid grid-rows-[auto_1fr_auto] items-center gap-4 bg-background-primary">
+<main class="min-h-screen w-screen p-2 grid grid-rows-[auto_1fr_auto] gap-4 bg-background-primary">
     <header class="p-4 flex flex-col gap-1">
         <h1 class="text-body-large">Vero's camera roll</h1>
         <h2>veronica has a bunch of cameras, here, all of the photographies she has ever taken have been listed into different camera rolls</h2>
     </header>
-
-    <section class="px-2 mx-auto max-w-md">
+    <section class="px-2 flex flex-wrap justify-center gap-2">
         @forelse ($photographyPosts as $post)
-        <article class="flex flex-col gap-8 items-center justify-center">
-            <div class="flex flex-col gap-4 items-center">
-                <img class="max-h-48 max-w-48 w-auto h-auto" src="{{ asset('storage/' . $post->icon->path) }}">
-                <h1 class="text-center text-body-medium leading-tight">{{ $post->title }}</h1>
-                <p class="text-center">{{ $post->description }}</p>
-            </div>
-            <x-button href="{{ route('camera.roll', $post) }}">Open this Camera Roll</x-button>
-        </article>
+	<article class="h-fit w-24 p-2 hover:outline-2 hover:outline-dashed hover:outline-foreground/10 cursor-pointer">
+    	<a class="flex flex-col items-center gap-1" href="{{ route('camera.roll', $post) }}">
+        	<img class="size-10" src="{{ asset('storage/' . $icon) }}" />
+        	<div class="wrap-break-word text-center text-sm">
+            		<span>{{ $post->title }}</span>
+            		<span>{{ $post->date }}</span>
+        <	/div>
+    </a>
+</article>
         @empty
         <p>No camera rolls yet, check back later!</p>
         @endforelse
     </section>
-
     <section class="p-4">
-        {{ $photographyPosts->links('components.pagination', ['direction' => 'vertical']) }}
+        {{ $posts->links('components.pagination') }}
     </section>
 </main>
 @endsection
