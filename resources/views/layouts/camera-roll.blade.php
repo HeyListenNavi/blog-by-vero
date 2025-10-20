9a3aa266aede595713881e5cd5de4d69b91c463f
Verónica <heylistennavi@proton.me>
2025-10-20 01:32:28 -0700

    Remove min-w-lg class from main element

--- resources/views/layouts/camera-roll.blade.php
+++ resources/views/layouts/camera-roll.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('body')
-<main class="min-w-0 min-w-lg w-full bg-background-primary min-h-svh p-4 flex flex-col gap-4">
+<main class="min-w-0 w-full bg-background-primary min-h-svh p-4 flex flex-col gap-4">
     <a class="self-start underline cursor-pointer hover:text-highlight-secondary transition-colors" onclick="history.back()">← go back to the camera</a>
 
     <section class="max-w-4xl self-center flex flex-col gap-2">
