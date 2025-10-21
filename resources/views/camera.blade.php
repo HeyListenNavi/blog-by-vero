a45c4850c0a62fc710239ab912aac32ab73c6c3a
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2025-10-21 00:08:05 -0700

    Fix posts icon sizes in responsive

--- resources/views/camera.blade.php
+++ resources/views/camera.blade.php
@@ -8,10 +8,10 @@
     </header>
     <section class="px-2 flex flex-wrap content-start gap-2">
         @forelse ($photographyPosts as $post)
-	<article class="h-fit w-24 p-2 hover:outline-2 hover:outline-dashed hover:outline-foreground/10 cursor-pointer">
+	<article class="h-fit w-18 lg:w-24 p-1 lg:p-2 hover:outline-2 hover:outline-dashed hover:outline-foreground/10 cursor-pointer">
     	<a class="flex flex-col items-center gap-1" href="{{ route('camera.roll', $post) }}">
         	<img class="size-10" src="{{ asset('storage/' . $post->icon->path) }}" />
-        	<div class="wrap-break-word text-center text-sm">
+        	<div class="wrap-break-word text-center text-xs lg:text-sm">
             		<span>{{ $post->title }}</span>
             		<span>{{ $post->date }}</span>
         	</div>
--- resources/views/components/post-icon.blade.php
+++ resources/views/components/post-icon.blade.php
@@ -5,10 +5,10 @@
     'date',
 ])
 
-<article class="h-fit w-24 p-2 hover:outline-2 hover:outline-dashed hover:outline-foreground/10 cursor-pointer">
+<article class="h-fit w-18 lg:w-24 p-1 lg:p-2 hover:outline-2 hover:outline-dashed hover:outline-foreground/10 cursor-pointer">
     <a class="flex flex-col items-center gap-1" href="{{ $href }}" target="_parent">
         <img class="size-10" src="{{ asset('storage/' . $icon) }}" />
-        <div class="wrap-break-word text-center text-sm">
+        <div class="wrap-break-word text-center text-xs lg:text-sm">
             <span>{{ $title }}</span>
             <span>{{ $date }}</span>
         </div>
