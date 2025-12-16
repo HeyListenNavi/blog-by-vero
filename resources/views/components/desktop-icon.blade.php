b89a396836fd178058fdde075797a545cfa59a77
Verónica Guzman Ochoa <heylistennavi@proton.me>
2025-12-16 01:36:09 -0800

    update: now desktop icon component can be an external link

--- resources/views/components/desktop-icon.blade.php
+++ resources/views/components/desktop-icon.blade.php
@@ -8,6 +8,7 @@
     'size' => fake()->numberBetween(0, 999),
     'date' => fake()->date(),
     'location' => '/home/naviheylisten/projects/blog-by-vero',
+    'link' => null
 ])
 
 @pushOnce('scripts')
@@ -72,40 +73,72 @@
     </script>
 @endPushOnce
 
-<article
-    x-data="{ id: $id('icon') }"
-    x-init="
-        @if($open)
-        $store.windowManager.spawn($refs.app);
-        @endif
-        $nextTick(() => makeIconDraggable(id));
-    "
+@if(!$link)
+    <article
+        x-data="{ id: $id('icon') }"
+        x-init="
+            @if($open)
+            $store.windowManager.spawn($refs.app);
+            @endif
+            $nextTick(() => makeIconDraggable(id));
+        "
 
-    x-on:click="$store.windowManager.spawn($refs.app)"
-    x-on:contextmenu.prevent="$store.windowManager.spawn($refs.properties)"
-    x-on:long-press.prevent="$store.windowManager.spawn($refs.properties)"
+        x-on:click="$store.windowManager.spawn($refs.app)"
+        x-on:contextmenu.prevent="$store.windowManager.spawn($refs.properties)"
+        x-on:long-press.prevent="$store.windowManager.spawn($refs.properties)"
 
-    x-bind:id="id"
-    class="icon min-h-20 w-18 py-2 px-1 flex flex-col gap-2 items-center justify-center select-none hover:bg-background-primary/30 hover:text-highlight-secondary"
->
-    <img class="size-10" src="{{ $icon }}">
-    <p class="w-full text-center wrap-break-word text-[10px] text-shadow-outline text-shadow-background-primary/60">{{ $name }}</p>
+        x-bind:id="id"
+        class="icon min-h-20 w-18 py-2 px-1 flex flex-col gap-2 items-center justify-center select-none hover:bg-background-primary/30 hover:text-highlight-secondary"
+    >
+        <img class="size-10" src="{{ $icon }}">
+        <p class="w-full text-center wrap-break-word text-[10px] text-shadow-outline text-shadow-background-primary/60">{{ $name }}</p>
 
-    <template x-ref="app" name="{{ $name }}">
-        <x-window.desktop name="{{ $name }}" {{ $attributes->merge() }}>
-            {{ $slot }}
-        </x-window.desktop>
-    </template>
+        <template x-ref="app" name="{{ $name }}">
+            <x-window.desktop name="{{ $name }}" {{ $attributes->merge() }}>
+                {{ $slot }}
+            </x-window.desktop>
+        </template>
 
-    <template x-ref="properties" name="{{ $name }} Properties">
-        <x-window.properties
-            name="{{ $name }} Properties"
-            icon="{{ $icon }}"
-            extension="{{ $extension }}"
-            description="{{ $description }}"
-            size="{{ $size }}"
-            date="{{ $date }}"
-            location="{{ $location }}"
-        />
-    </template>
-</article>
+        <template x-ref="properties" name="{{ $name }} Properties">
+            <x-window.properties
+                name="{{ $name }} Properties"
+                icon="{{ $icon }}"
+                extension="{{ $extension }}"
+                description="{{ $description }}"
+                size="{{ $size }}"
+                date="{{ $date }}"
+                location="{{ $location }}"
+            />
+        </template>
+    </article>
+@else
+    <article
+        x-data="{ id: $id('icon') }"
+        x-init="$nextTick(() => makeIconDraggable(id));"
+
+        x-on:click="window.open('{{ $link }}', '_blank');"
+        x-on:contextmenu.prevent="$store.windowManager.spawn($refs.properties)"
+        x-on:long-press.prevent="$store.windowManager.spawn($refs.properties)"
+
+        x-bind:id="id"
+        class="icon min-h-20 w-18 py-2 px-1 flex flex-col gap-2 items-center justify-center select-none hover:bg-background-primary/30 hover:text-highlight-secondary"
+    >
+        <div class="relative">
+            <img class="size-10 absolute -bottom-1 -left-1" src="{{ Vite::image('icons/shortcut.png') }}">
+            <img class="size-10" src="{{ $icon }}">
+        </div>
+        <p class="w-full text-center wrap-break-word text-[10px] text-shadow-outline text-shadow-background-primary/60">{{ $name }}</p>
+
+        <template x-ref="properties" name="{{ $name }} Properties">
+            <x-window.properties
+                name="{{ $name }} Properties"
+                icon="{{ $icon }}"
+                extension="{{ $extension }}"
+                description="{{ $description }}"
+                size="{{ $size }}"
+                date="{{ $date }}"
+                location="{{ $location }}"
+            />
+        </template>
+    </article>
+@endif
