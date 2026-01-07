775c75052f6fd6b63323cdd0c3610cda8b617c3d
Verónica Guzman Ochoa <heylistennavi@proton.me>
2026-01-07 05:22:56 -0800

    delete: unused component

--- resources/views/components/icon-option.blade.php
+++
@@ -1,6 +0,0 @@
-<div style="display: flex; align-items: center; gap: 10px;">
-    <img src="{{ asset('storage/' . $icon->path) }}"
-         alt="{{ $icon->name }}"
-         style="width: 48px; height: 48px; object-fit: cover;">
-    <span>{{ $icon->name }}</span>
-</div>
