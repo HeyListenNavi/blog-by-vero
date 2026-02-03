70aae151b57c0b11cdbb17821a8abe9a881154d6
Verónica Guzman Ochoa <heylistennavi@proton.me>
2026-02-02 18:19:34 -0800

    style: fix disabled style of the button component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -4,11 +4,11 @@
 ])
 
 @if ($href)
-    <a href="{{ $href }}" {{ $attributes->merge(['class' => 'bg-background-tertiary px-2 py-1 pb-1.5 border-2 border-foreground/30 [border-style:inset] active:border-dashed']) }}>
+    <a href="{{ $href }}" {{ $attributes->merge(['class' => 'bg-background-tertiary px-2 py-1 pb-1.5 border-2 border-foreground/30 [border-style:inset] active:border-dashed disabled:text-zinc-600 disabled:cursor-not-allowed disabled:bg-background-tertiary disabled:border-foreground/30 disabled:[border-style:inset]']) }}>
         {{ $slot }}
     </a>
 @elseif ($type)
-    <button type="{{ $type }}" {{ $attributes->merge(['class' => 'bg-background-tertiary px-2 py-1 pb-1.5 border-2 border-foreground/30 [border-style:inset] active:border-dashed cursor-pointer']) }}>
+    <button type="{{ $type }}" {{ $attributes->merge(['class' => 'bg-background-tertiary px-2 py-1 pb-1.5 border-2 border-foreground/30 [border-style:inset] active:border-dashed cursor-pointer disabled:text-zinc-600 disabled:cursor-not-allowed disabled:bg-background-tertiary disabled:border-foreground/30 disabled:[border-style:inset]']) }}>
         {{ $slot }}
     </button>
 @else
