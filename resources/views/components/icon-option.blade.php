a34d9ed1f8ba36e5797424611cc6e7e907981a74
Verónica Guzman Ochoa <heylistennavi@proton.me>
2026-04-10 01:53:57 -0700

    Revert "delete: unused component"
    This reverts commit 775c75052f6fd6b63323cdd0c3610cda8b617c3d.

---
+++ resources/views/components/icon-option.blade.php
@@ -0,0 +1,6 @@
+<div style="display: flex; align-items: center; gap: 10px;">
+    <img src="{{ asset('storage/' . $icon->path) }}"
+         alt="{{ $icon->name }}"
+         style="width: 48px; height: 48px; object-fit: cover;">
+    <span>{{ $icon->name }}</span>
+</div>
