78a0b25879949761b85a7256c696b0549eb77e69
Verónica Guzman Ochoa <heylistennavi@proton.me>
2026-04-10 23:57:41 -0700

    add: media table and model

---
+++ app/Models/Media.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Media extends Model
+{
+    protected $casts = [
+        'review_date' => 'date',
+        'is_favorite' => 'boolean',
+    ];
+
+    protected $fillable = [
+        'title',
+        'type',
+        'poster',
+        'stars',
+        'is_favorite',
+        'review_date',
+        'content',
+    ];
+}
---
+++ database/migrations/2026_04_10_101040_create_media_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->enum('type', ['movie', 'videogame', 'show'])->default('movie');
+            $table->string('poster')->nullable();
+            $table->integer('stars')->default(0);
+            $table->boolean('is_favorite')->default(false);
+            $table->date('review_date')->nullable();
+            $table->text('content')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('media');
+    }
+};
