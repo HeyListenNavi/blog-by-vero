cf2d4d53a6a8804603abc9644a68f75404f95562
Verónica Guzman Ochoa <heylistennavi@proton.me>
2026-04-11 22:58:30 -0700

    style: update post markdown to use prose class

--- resources/views/layouts/post.blade.php
+++ resources/views/layouts/post.blade.php
@@ -3,10 +3,12 @@
 @section('body')
 <main class="w-screen bg-background-primary min-h-svh p-8 flex flex-col gap-4">
     <a class="underline cursor-pointer hover:text-highlight-secondary transition-colors" href="{{ route('home') }}">← {{ $post->date }}</a>
-    
+
     <section class="w-full max-w-2xl self-center flex flex-col gap-2">
         <h1 class="text-display-medium text-4xl">{{ $post->title }}</h1>
-        @markdown($post->content)
+        <div class="w-full max-w-full prose dark:prose-invert text-xs text-foreground/80 leading-6">
+            @markdown($post->content)
+        </div>
     </section>
 </main>
 @endsection
