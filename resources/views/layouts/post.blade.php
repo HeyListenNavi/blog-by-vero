e69876f61cec55dcd07cd3b91e0e705593498f42
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-06-04 01:22:59 -0700

    style(view): change post styles to make it look more like a redacted file

--- resources/views/layouts/post.blade.php
+++ resources/views/layouts/post.blade.php
@@ -1,49 +1,143 @@
 @extends('layouts.app')
 
 @section('body')
-<main class="w-screen bg-background min-h-svh px-4 pt-4 pb-16 flex flex-col gap-4">
-    <a
-        class="underline cursor-pointer hover:text-highlight-secondary transition-colors"
-        href="{{ route('home') }}"
-        id="top"
-    >
-        ← {{$post->date }}
-    </a>
-
-    <section class="w-full max-w-2xl self-center flex flex-col gap-2">
-        <x-window title="Post {{ $post->date }}" :buttons="['minimize', 'maximize', 'close']">
-            <div class="px-8 py-6 flex flex-col gap-4">
-                <h1 class="text-display-medium text-4xl break-words">{{ $post->title }}</h1>
-                <div class="w-full max-w-full prose dark:prose-invert text-xs text-foreground/80 leading-6">
-                    @markdown($post->content)
-                </div>
-            </div>
-        </x-window>
-    </section>
-
-    <x-window 
-        class="w-full max-w-md fixed bottom-2 left-1/2 -translate-x-1/2 scale-80 lg:scale-100"
-        title="NAVI-gation"
-    >
-        <div class="flex justify-between gap-4 py-2 px-4">
-            <div class="flex justify-start">
-                @if($prev = $post->previous())
-                <x-button href="{{ route('journal.post', $prev) }}">← Previous</x-button>
-            @else
-            <x-button disabled>← Previous</x-button>
-            @endif
-            </div>
-            
-            <x-button href="#top">Top ^</x-button>
-            
-            <div class="flex justify-end">
-                @if($next = $post->next())
-                <x-button href="{{ route('journal.post', $next) }}">Next →</x-button>
-                @else
-                <x-button disabled>Next →</x-button>
-                @endif
+    <main class="flex min-h-svh w-screen flex-col gap-4 bg-white px-0 pb-20 pt-4 lg:px-4">
+        <div
+            class="drop-shadow-black/20 before:drop-shadow-black/20 scale-80 w-fit -rotate-3 font-serif italic drop-shadow-md before:fixed before:-top-2 before:left-0 before:-z-10 before:size-10 before:rounded-sm before:bg-[#97ddff] before:drop-shadow-md before:content-[''] lg:scale-100">
+            <div class ="flex justify-between gap-4 rounded-sm bg-[#6bccf9] px-3 py-2">
+                <a
+                    class="text-indigo-600 transition-colors hover:text-indigo-800 hover:underline"
+                    id="top"
+                    href="{{ route('home') }}"
+                >
+                    ← {{ $post->date->format('d / m / Y') }}
+                </a>
             </div>
         </div>
-    </x-window>
-</main>
+
+        <div
+            class="bg-cutting-mat drop-shadow-black/40 3xl:py-32 flex w-full max-w-5xl flex-col items-center self-center rounded-3xl px-6 py-8 drop-shadow-xl lg:rotate-[0.5deg] lg:px-12"
+            style="
+                background-position: top center, bottom center, center center;
+                background-repeat: no-repeat, no-repeat, repeat-y;
+                background-size: 100% auto
+            "
+        >
+            <section
+                class="paper-bg animate-paper-throw flex max-w-3xl flex-col gap-12 px-6 py-8 opacity-0 shadow-xl shadow-black/40 lg:-rotate-[0.6deg] lg:px-12 lg:py-14"
+                style="animation-delay: 400ms"
+            >
+                <div class="text-background-tertiary flex flex-col gap-2 break-words text-center">
+                    <div>
+                        <u><i>heylistennavi@proton.me</i></u>
+                        <p class="text-body-small">Aperture Science Research Facility</p>
+                    </div>
+                    <h1 class="text-display-medium text-4xl">{{ $post->title }}</h1>
+                </div>
+
+                <header class="text-background-primary grid grid-cols-2 justify-between gap-4">
+                    <p>REPORT: <b>#{{ sprintf('%05d', $post->id) }}</b></p>
+                    <p>PUBLISHED: <b>{{ $post->created_at }}</b></p>
+
+                    <p>DATE: <b>{{ $post->date->format('l, F j, Y') }}</b></p>
+                    <p>ORIGINALLY KNWON AS: <b>[ REDACTED ]</b></p>
+
+                    <p class="col-span-2">SUBJECT: <b><i>[ {{ $post->slug }} ]</i></b></p>
+                </header>
+
+                <div class="prose w-full max-w-full text-xs leading-6">
+                    @markdown($post->content)
+                </div>
+
+                <footer class="flex items-center justify-between">
+                    <div class="text-background-primary scale-80 lg:scale-100">
+                        <p>
+                            ALL INFORMATION CONTAINED <br />
+                            HERE IS
+                            <span class="relative">
+                                UNCLASSIFIED
+                                <img
+                                    class="absolute -top-2.5 left-0 h-8 w-fit opacity-95"
+                                    src="{{ Vite::image('marker-line-texture/1.png') }}"
+                                />
+                            </span>
+                        </p>
+                        <p>
+                            DATE: <b><i><u>{{ $post->date }}</u></i></b>
+                        </p>
+                        <p>
+                            BY:
+                            <span class="relative">
+                                NAVIHEYLISTEN
+                                <img
+                                    class="absolute -top-1 left-0 h-6 w-fit opacity-95"
+                                    src="{{ Vite::image('marker-line-texture/2.png') }}"
+                                />
+                            </span>
+                        </p>
+                    </div>
+
+                    <div class="text-background-primary scale-80 lg:scale-100">
+                        <p class="text-center">WRITTEN AND APPROVED BY:</p>
+                        <div class="relative">
+                            <img
+                                class="w-52"
+                                src="{{ Vite::image('signature.png') }}"
+                            />
+                            <img
+                                class="absolute left-0 top-3 opacity-95"
+                                src="{{ Vite::image('marker-line-texture/3.png') }}"
+                            />
+                        </div>
+                        <span class="my-2 block h-0.5 w-full bg-black"></span>
+                        <p class="text-center font-bold">[ REDACTED ]</p>
+                    </div>
+                </footer>
+            </section>
+        </div>
+
+        <div
+            class="drop-shadow-black/20 before:drop-shadow-black/20 scale-80 fixed bottom-4 left-1/2 w-full max-w-md -translate-x-1/2 font-serif italic drop-shadow-md before:fixed before:-top-2 before:left-0 before:-z-10 before:size-10 before:rounded-sm before:bg-[#ffd485] before:drop-shadow-md before:content-[''] lg:scale-100">
+            <nav class="flex justify-between gap-4 rounded-sm bg-[#ffe1a8] px-4 py-3">
+                @if ($prev = $post->previous())
+                    <a
+                        class="text-indigo-600 transition-colors hover:text-indigo-800 hover:underline"
+                        href="{{ route('journal.post', $prev) }}"
+                    >
+                        ← Previous
+                    </a>
+                @else
+                    <span
+                        class="text-gray-700"
+                        href=""
+                    >
+                        ← No more
+                    </span>
+                @endif
+
+                <a
+                    class="text-indigo-600 transition-colors hover:text-indigo-800 hover:underline"
+                    href="#top"
+                >
+                    Top ^
+                </a>
+
+                @if ($next = $post->next())
+                    <a
+                        class="text-indigo-600 transition-colors hover:text-indigo-800 hover:underline"
+                        href="{{ route('journal.post', $next) }}"
+                    >
+                        Next →
+                    </a>
+                @else
+                    <span
+                        class="text-gray-700"
+                        href=""
+                    >
+                        No more →
+                    </span>
+                @endif
+            </nav>
+        </div>
+    </main>
 @endsection
